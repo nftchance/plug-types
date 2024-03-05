5c88dfe106faf2253f32820e4386c601f0bba321
nftchance <chance@utc24.io>
2024-03-04 23:51:38 -0600

    chore: exporting everything

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,43 @@
-export { config } from './core/config'
+export { config } from '@/core/config'
+export {
+	getSolidity,
+	getEncodedValueFor,
+	getDigestGetterName,
+	getSignerGetterName,
+	getPacketHashGetters,
+	getPacketHashGetterName,
+	getArrayPacketHashGetter,
+	generate
+} from '@/core/sol'
+
 export {
 	BASE_AUTH,
 	constants,
 	EIP712_TYPES,
 	PLUGS_TYPES,
 	LIVE_PLUGS_TYPES
-} from './lib/constants'
+} from '@/lib/constants'
+
+export {
+	Address,
+	AddressType,
+	Bytes32,
+	Bytes32Type,
+	Bytes,
+	BytesType
+} from '@/zod/types'
+
+export {
+	Plugs,
+	PlugsSchema,
+	Plug,
+	PlugSchema,
+	Fuse,
+	FuseSchema,
+	Current,
+	CurrentSchema,
+	LivePlugs,
+	LivePlugsSchema,
+	EIP712Domain,
+	EIP712DomainSchema
+} from '@/zod/index'
