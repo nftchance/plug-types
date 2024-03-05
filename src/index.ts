38e5ab4d1c58c1d1f81db3ba106508e062fcb925
nftchance <chance@utc24.io>
2024-03-04 23:45:08 -0600

    fix: include types in package export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { config } from './core/config'
-export { BASE_AUTH, constants } from './lib/constants'
+export {
+	BASE_AUTH,
+	constants,
+	EIP712_TYPES,
+	PLUGS_TYPES,
+	LIVE_PLUGS_TYPES
+} from './lib/constants'
